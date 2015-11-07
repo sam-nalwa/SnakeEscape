dac284843a67969bec64244876e99c665ffaad26
= <=>
2015-11-07 09:58:57 -0800

    Integrated socket.io

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var socket_io    = require( "socket.io" );
 
 var routes = require('./routes/index');
 
 
 var app = express();
 
+var io           = socket_io();
+app.io           = io;
 // view engine setup
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
@@ -22,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', routes.getRouter(app.io));
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
